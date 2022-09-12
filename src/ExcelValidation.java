81e6ce018f28dccdf91f2bfb737d129b0f06197f
asif <khondoker.akter@reddotdigitalit.com>
2022-09-12 14:53:22 +0600

    new file for validation

--- src/ExcelValidation.java
+++ src/ExcelValidation.java
@@ -2,8 +2,11 @@ import org.apache.poi.ss.usermodel.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ExcelValidation {
+
     public static void main(String[] args) throws Exception {
         //String filePath = "PRESHOW_2021-12-10_12-00-00.370_SameDay_SQLDAG_POW.xlsx";
         //String filePath = "test.xlsx";
@@ -20,15 +23,19 @@ public class ExcelValidation {
             }
         }
 
+        Validator validator = new Validator();
+
+        System.out.println("is valid phone: " + validator.isValidPhoneNumber("1718161052"));
 
         if(workbook != null){
 
             Sheet sheet = workbook.getSheetAt(0);
             for (Row row : sheet) {
+                //System.out.println("Row: " + row.getRowNum());
                 for(Cell cell : row){
                     switch (cell.getCellType()){
                         case STRING:
-                            System.out.println(cell.getStringCellValue());
+                            System.out.println("row: " + row.getRowNum() + " cell: " + cell.getColumnIndex() + " - " + cell.getStringCellValue());
                             break;
                         case BLANK:
                             break;
@@ -51,4 +58,5 @@ public class ExcelValidation {
         }
 
     }
+
 }
